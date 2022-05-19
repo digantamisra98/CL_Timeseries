9d9fb133d6f2ec1fe54667c7cf924cb72e399d19
Kashif Rasul <kashif.rasul@gmail.com>
2022-05-19 15:16:03 +0200

    added solver

--- dgr/models.py
+++ dgr/models.py
@@ -42,5 +42,20 @@ class Generator(nn.Module):
 
 # class Solver (define your continual learner here):
 class Solver(nn.Module):
-    def __init__(self, sequence_length, hidden_sizes, classes):
-        pass
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1):
+        super(Solver, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
